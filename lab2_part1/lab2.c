6332bbf81fef5dbf7aea711fd58e4d4cfe54db28
Grigor Ambartsumyan <ambartsumyan.grigor@gmail.com>
2016-01-31 18:48:59 -0800

    removed extra files and added comments

--- lab2_part1/lab2.c
+++ lab2_part1/lab2.c
@@ -13,7 +13,7 @@ int main(){
 
 	char buffer[16];
 	int i;
-	int valid = 1;
+	int valid = 1; // valid flag
 	unsigned long long int num;
 	while(1){
 		printf("Please enter a 32 bit integer (q to quit):");
@@ -21,21 +21,21 @@ int main(){
 		//gets(buffer);
 		
 		if(buffer[0] == 'q')
-			break;	
+			break;	// exit on q
+
 		for(i = 0; i < strlen(buffer); i++)
-		{
 			if(!isdigit(buffer[i]))
 			{
 				valid = 0;
 				break;
-			}	
-		}
+			}	// check each digit
+
 		if(valid == 0)
 			printf("Invalid character. Try again\n");
 		else	// convert to int
 		{	
 			num = atoll(buffer);
-			if(num <= 4294967295)
+			if(num <= 4294967295) // largest 32 bit int
 				*(reg) = num;
 			else
 				printf("Number too big\n");
